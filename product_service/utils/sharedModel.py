041f841d1277ca298141cb59987fb3b3cb5b0453
AnhQuan29112004 <namcaodidong2004@gmail.com>
2025-06-25 01:05:50 +0700

    Feat: shared model

--- product_service/utils/sharedModel.py
+++ product_service/utils/sharedModel.py
@@ -4,8 +4,8 @@ class SharedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
-    created_by = models.IntegerField()
-    updated_by = models.IntegerField()
+    created_by = models.IntegerField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
 
     class Meta:
         abstract = True
