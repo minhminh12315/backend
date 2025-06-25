3e1bcffe4155c00a5cbe8420ad86534586a0386a
AnhQuan29112004 <namcaodidong2004@gmail.com>
2025-06-25 21:33:12 +0700

    Feat: add logic to load js, css to website

--- product_service/product_service/urls.py
+++ product_service/product_service/urls.py
@@ -30,3 +30,5 @@ urlpatterns = [
 # Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
