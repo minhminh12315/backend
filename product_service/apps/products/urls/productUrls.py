85d7b8459d925ce0522c358c48a819d6f31cc276
AnhQuan29112004 <namcaodidong2004@gmail.com>
2025-06-25 21:28:04 +0700

    Feat: add endpoint for get variant by sku

--- product_service/apps/products/urls/productUrls.py
+++ product_service/apps/products/urls/productUrls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from apps.products.views.productViews import GetAllVariantView,GetRecommandProductsView
+from apps.products.views.productViews import GetVariantBySku, GetAllVariantView,GetRecommandProductsView
 
 urlpatterns = [
     path('list_products', GetAllVariantView.as_view(), name='get_all_products'),
     path('recommand_products', GetRecommandProductsView.as_view(), name='get_recommand_products'),
+    path('get_product/<str:sku>', GetVariantBySku.as_view(), name='get_product_by_sku'),
 ]
